dd50e3f0d8767e943ba539bd7d257b46bfc2ab8b
LordVol <pletana@pletana.co.uk>
2015-07-06 00:26:23 +0100

    Done a tonne of updates
    But still need to work in the soapfault exception class

--- src/Shipping/ShippingService.php
+++ src/Shipping/ShippingService.php
@@ -15,4 +15,84 @@ class ShippingService
             return false;
         }
     }
+    
+    public function getDomesticServicesByPostcode($postcode=null)
+    {
+        $getDomesticServicesByPostcodeResult = false;
+        if (!$postcode) {
+            return $getDomesticServicesByPostcodeResult;
+        }
+        try {
+            $getDomesticServicesByPostcodeResult = $this->soapClient->GetDomesticServicesByPostcode($postcode);
+        } catch(Exception $e) {
+            return $e->getMessage();
+        }
+        return $getDomesticServicesByPostcodeResult;
+    }
+    
+    public function getDomesticServices()
+    {
+        $getDomesticServicesResult = false;
+        try {
+            $getDomesticServicesResult = $this->soapClient->GetDomesticServices();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $getDomesticServicesResult;
+    }
+    
+    public function getShipment($shipmentID=null)
+    {
+        $getShipmentResult = false;
+        if (!$shipmentID) {
+            return $getShipmentResult;
+        }
+        try {
+            $getShipmentResult = $this->soapClient->GetShipment($shipmentID);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $getShipmentResult;
+    }
+    
+    /**
+     * Adds a domestic shipment to the shipment queue
+     * 
+     * @param array $domesticShipmentDetailsArray
+     * @return string $shipmentID
+     * 
+     $domesticShipmentDetailsArray should contain the following elements:
+     ServiceID
+     OrderReference
+     Contents
+     ParcelQuantity
+     CompanyName
+     RecipientName
+     Street
+     Locality
+     Town
+     County
+     Postcode
+     RecipientEmail
+     EmailNotification (0 = false, 1 = true)
+     DashboardNotification (0 = false, 1 = true)
+     * 
+     */
+    public function addDomesticShipment($domesticShipmentDetailsArray=null)
+    {
+        $addDomesticShipmentResult = false;
+        if (!$domesticShipmentDetailsArray) {
+            return $addDomesticShipmentResult;
+        }
+        try {
+            $shipment = new StdClass();
+            foreach ($addDomesticShipmentDetailsArray as $shipmentDetailKey => $shipmentDetailValue) {
+                $shipment->$shipmentDetailKey = $shipmentDetailValue;
+            }
+            $addDomesticShipmentResult = $this->soapClient->AddDomesticShipment($shipment);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $addDomesticShipmentResult;
+    }
 }
--- src/Tracking/TrackingService.php
+++ src/Tracking/TrackingService.php
@@ -15,4 +15,23 @@ class TrackingService
             return false;
         }
     }
+    
+    /**
+     * Gets tracking info for a tracking number
+     * @param string $trackingNumber
+     * @return array $getTrackingResult
+     */
+    public function getTracking($trackingNumber=null)
+    {
+        $getTrackingResult = false;
+        if (!$trackingNumber) {
+            return $getTrackingResult;
+        }
+        try {
+            $getTrackingResult = $this->soapClient->GetTracking($trackingNumber);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $getTrackingResult;
+    }
 }
